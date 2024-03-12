0c6839e59717e85d8e2e0e4cc57a9424c64dbb13
Vaddemani Siddharth <139576040+siddharthcode98@users.noreply.github.com>
2024-03-12 16:10:05 +0530

    first commit

---
+++ src/component/massage/ChatBoxLeftBar.jsx
@@ -0,0 +1,17 @@
+import SidebarHeader from "./SidebarHeader";
+import PinnedMassages from "./PinnedMassages";
+import AllMessages from "./AllMessages";
+
+function ChatBoxLeftBar() {
+  return (
+    <aside className="h-full 2xl:col-span-3 xl:col-span-4 lg:col-span-5 bg-white dark:bg-darkblack-600 border border-bgray-200 dark:border-darkblack-400 pr-7 pl-12 pt-6 pb-10 hidden lg:block">
+      <SidebarHeader />
+      <div className="pt-6">
+        <PinnedMassages />
+        <AllMessages />
+      </div>
+    </aside>
+  );
+}
+
+export default ChatBoxLeftBar;
